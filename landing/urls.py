aa82a594d3b9dc9d1ff7b3eed85497338cd38071
AndresCW <andres@cpsingenieria.co>
2015-02-08 01:37:26 -0500

    Agregado el footer

--- landing/urls.py
+++ landing/urls.py
@@ -3,4 +3,5 @@ from landing import views
 
 urlpatterns = patterns('',
 		url(r'^$', views.home, name='home'),
+		url(r'^contactar/$', views.contactar, name='contactar')
 	)
--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 def home(request):
 	return render(request, 'landing/landing.html')
+
+def contactar(request):
+	return HttpResponseRedirect(reverse('landing:home'))
